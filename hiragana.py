547fbe760773af8cbbb24e1005bf61d6a8c729bb
leehyowon14 <kingtv347@gmail.com>
2023-12-10 21:23:08 +0900

    first commit

---
+++ hiragana.py
@@ -0,0 +1,56 @@
+from re import sub as regex_replace
+from datasets import load_dataset, Dataset
+from requests import get as http_get
+from tqdm import tqdm
+
+total_letter_counter: int = 0
+
+# 카타가나 - 히라가나 사전 만들기 / 히라가나 - 개수 사전 만들기 / 모든 히라가나가 담긴 리스트 만들기
+KATA_TO_HIRA: dict[str, str] = {}
+hira_counter: dict[str, int] = {}
+HIRA_LIST: list[str] = []
+# 히라가나 유니코드 시작점: 12353
+# 히라가나 유니코드 종료점: 12438
+# 카타가나 유니코드 시작점: 12449
+for hira_unicode in range(12353, 12439):
+    hira = chr(hira_unicode)
+    KATA_TO_HIRA[chr(12449 + (hira_unicode - 12353))] = hira
+    hira_counter[hira] = 0
+    HIRA_LIST.append(hira)
+
+# 일본어 데이터셋 불러오기
+print("Start Loading Dataset...")
+dataset: Dataset = load_dataset("izumi-lab/llm-japanese-dataset", revision="main")
+print("Done.")
+try:
+    for data in tqdm(dataset["train"]):
+        string: str = ''
+        for key in data:
+            if data[key] != '':
+                string += data[key]
+
+        # 모든 글자 히라가나로 바꾼 후 히라가나만 남기기
+        res: list = list(http_get(
+            f"https://learn-language.tokyo/api/toHiraganaKanjiKatakana?text={string}&toLang=toFuriganaNormal",
+            timeout=30
+        ).text)
+        for i, letter in enumerate(res):
+            res[i] = KATA_TO_HIRA.get(letter, letter)
+        res: str = ''.join(res)
+        res = regex_replace("[^ぁ-ん]", "", res)
+
+        for letter in res:
+            total_letter_counter += 1
+            hira_counter[letter] += 1
+finally:
+    for i in range(len(HIRA_LIST) - 1, 0, -2):
+        hira_counter[HIRA_LIST[i]] += hira_counter[HIRA_LIST[i - 1]]
+        del hira_counter[HIRA_LIST[i - 1]]
+
+    with open("./result.txt", "wt+", encoding="UTF8") as file:
+        file.write(f"---RESULT(total {total_letter_counter} letter)---\n")
+        for hira in hira_counter:
+            file.write(f"{hira}: {hira_counter[hira]}\n")
+        file.write("---END---\n\n\n")
+
+    print("Result is saved in 'result.txt'")
---
+++ include_kanji.py
@@ -0,0 +1,67 @@
+from re import sub as regex_replace
+from datasets import load_dataset, Dataset
+from tqdm import tqdm
+
+total_letter_counter: int = 0
+
+# 카타가나 - 히라가나 사전 만들기 / 히라가나 - 개수 사전 만들기 / 모든 히라가나가 담긴 리스트 만들기
+KATA_TO_HIRA: dict[str, str] = {}
+hira_counter: dict[str, int] = {}
+HIRA_LIST: list[str] = []
+# 히라가나 유니코드 범위: 12353 ~ 12438
+# 카타가나 유니코드 시작점: 12449 / 히라가나와 1대 1로 대응됨
+for hira_unicode in range(12353, 12439):
+    hira = chr(hira_unicode)
+    KATA_TO_HIRA[chr(12449 + (hira_unicode - 12353))] = hira
+    hira_counter[hira] = 0
+    HIRA_LIST.append(hira)
+
+# 싱용한자 & 표외한자 - 개수 사전 만들기
+# 상용한자 & 표외한자 유니코드 범위 : 19968 ~ 40911
+kanji_counter: dict[str, int] = {}
+for kanji_unicode in range(19968, 40911):
+    kanji_counter[chr(kanji_unicode)] = 0
+
+# 일본어 데이터셋 불러오기
+print("Start Loading Dataset...")
+dataset: Dataset = load_dataset("izumi-lab/llm-japanese-dataset", revision="main")
+print("Done.")
+try:
+    for data in tqdm(dataset["train"]):
+        string: str = ''
+        for key in data:
+            if data[key] != '':
+                string += data[key]
+        string: list = list(string)
+        for i, letter in enumerate(string):
+            string[i] = KATA_TO_HIRA.get(letter, letter)
+        string: str = ''.join(string)
+        string = regex_replace("[^ぁ-ん一-龯]", "", string)
+
+        for letter in string:
+            total_letter_counter += 1
+            if letter in hira_counter:
+                hira_counter[letter] += 1
+            else:
+                kanji_counter[letter] += 1
+finally:
+    for i in range(len(HIRA_LIST) - 1, 0, -2):
+        hira_counter[HIRA_LIST[i]] += hira_counter[HIRA_LIST[i - 1]]
+        del hira_counter[HIRA_LIST[i - 1]]
+
+    not_used_kanji: list[str] = []
+    for key in [kanji for kanji in kanji_counter]:
+        if kanji_counter[key] == 0:
+            del kanji_counter[key]
+            not_used_kanji.append(key)
+
+    with open("./result_kanji.txt", "wt+", encoding="UTF8") as file:
+        file.write(f"---RESULT(total {total_letter_counter} letter)---\n")
+        for hira in hira_counter:
+            file.write(f"{hira}: {hira_counter[hira]}\n")
+        for kanji in kanji_counter:
+            file.write(f"{kanji}: {kanji_counter[kanji]}\n")
+        file.write(f"Not used Kanji: {', '.join([kanji for kanji in not_used_kanji])}\n")
+        file.write("---END---\n\n\n")
+
+    print("Result is saved in 'result_kanji.txt'")
